55685046babc39c2f1b5245153f4c2ae1badb144
LucaMenestrina <menestrina.luca@gmail.com>
2025-12-09 06:30:20 -0500

    server

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    return f"For the given statement, the system response is {response}"
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
